1cb405a57b2d8ab4de4a6b75568b6e8ec73294df
Ringuma <doodlgirl863@gmail.com>
2019-03-25 22:07:35 -0400

    Tried out Sheetsu API
    But it didn't work and they have a limit of 20rows per sheet

--- js/recommendation.js
+++ js/recommendation.js
@@ -1,40 +1,12 @@
+    function successFunc(data) {
+      console.log(data);
+      document.getElementById("description").innerHTML = "Hello JavaScript!";
+    }
 
-const {google} = require('googleapis');
+    function errorFunc(e) {
+      console.log(e);
+      document.getElementById("description").innerHTML = e;
+    }
 
-exports.reply = (req, res) => {
-  var jwt = getJwt();
-  var apiKey = getApiKey();
-  var sheetID = "1brguO5nGfXS-Fr1Xcf3pqPTQoBUPGLTYM_EMAA9yJFw";
-  var range = "B2";
-
-  var recommendationTitle = readSheetRow(jwt, apiKey, sheetID, range);
-  console.log("Your new recommendation is " + recommendationTitle + ".");
-};
-
-function getApiKey() {
-  var apiKeyFile = require("../api_key.json");
-  return apiKeyFile.key;
-}
-
-function getJwt() {
-  var credentials = require("../credentials.json");
-  return new google.auth.JWT(
-    credentials.client_email, null, credentials.private_key,
-    ['https://www.googleapis.com/auth/spreadsheets']
-  );
-}
-
-function readSheetRow(jwt, apiKey, sheetID, range) {
-  const sheets = google.sheets({version: 'v4'});
-
-  var returnObject = sheets.spreadsheets.values.get({
-  spreadsheetId: sheetId,
-  range: range
-}).then((response) => {
-  var result = response.result;
-  var numRows = result.values ? result.values.length : 0;
-  console.log(`${numRows} rows retrieved.`);
-});
-
-  return returnObject.values[0][0];
-}
+    Sheetsu.read("https://sheetsu.com/apis/v1.0su/3e1d00ced9a4",
+     {}).then(successFunc, errorFunc);
