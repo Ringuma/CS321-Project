becf0614b9b13bc81911e60127685c24b7988a0f
Ringuma <doodlgirl863@gmail.com>
2019-03-25 21:47:06 -0400

    Trying to connect to Sheets API
    Connected to Sheets API and trying to add stuff. Probably gonna scrap this.

--- js/recommendation.js
+++ js/recommendation.js
@@ -1,2 +1,40 @@
 
-google.charts.load('current', {'packages':['corechart']});
+const {google} = require('googleapis');
+
+exports.reply = (req, res) => {
+  var jwt = getJwt();
+  var apiKey = getApiKey();
+  var sheetID = "1brguO5nGfXS-Fr1Xcf3pqPTQoBUPGLTYM_EMAA9yJFw";
+  var range = "B2";
+
+  var recommendationTitle = readSheetRow(jwt, apiKey, sheetID, range);
+  console.log("Your new recommendation is " + recommendationTitle + ".");
+};
+
+function getApiKey() {
+  var apiKeyFile = require("../api_key.json");
+  return apiKeyFile.key;
+}
+
+function getJwt() {
+  var credentials = require("../credentials.json");
+  return new google.auth.JWT(
+    credentials.client_email, null, credentials.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets']
+  );
+}
+
+function readSheetRow(jwt, apiKey, sheetID, range) {
+  const sheets = google.sheets({version: 'v4'});
+
+  var returnObject = sheets.spreadsheets.values.get({
+  spreadsheetId: sheetId,
+  range: range
+}).then((response) => {
+  var result = response.result;
+  var numRows = result.values ? result.values.length : 0;
+  console.log(`${numRows} rows retrieved.`);
+});
+
+  return returnObject.values[0][0];
+}
