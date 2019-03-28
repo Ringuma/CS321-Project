7265abc6688c7cb92405329514500204f5205fa9
Ringuma <doodlgirl863@gmail.com>
2019-03-27 23:08:09 -0400

    Connecting to local Dataset File
    Succesfully connected to local csv file with jquery ajax request. Added jquery and jquery.csv libraries to handle parsing the dataset. Got the first entry of the spreadsheet to appear in a popup.

--- js/recommendation.js
+++ js/recommendation.js
@@ -1,12 +1,24 @@
-    function successFunc(data) {
-      console.log(data);
-      document.getElementById("description").innerHTML = "Hello JavaScript!";
-    }
 
-    function errorFunc(e) {
-      console.log(e);
-      document.getElementById("description").innerHTML = e;
-    }
 
-    Sheetsu.read("https://sheetsu.com/apis/v1.0su/3e1d00ced9a4",
-     {}).then(successFunc, errorFunc);
+var sheetData = "";
+
+$.ajax({
+    type: "GET",
+    url: "../data/myAnimeListData.csv",
+    dataType: "text",
+    success: function(data) {
+        alert("worked");
+        sheetData = $.csv.toArrays(data);
+        alert(sheetData[0][0]);
+    },
+    error: function (request, status, error) {
+        alert(request.responseText);
+    }
+ });
+
+function computeRandomRecommendation(data) {
+
+}
+
+//var clientID = "983014862567-srsvv1p8djudqjalo4onabl8qog48mdi.apps.googleusercontent.com";
+//var spreadsheetID = "1brguO5nGfXS-Fr1Xcf3pqPTQoBUPGLTYM_EMAA9yJFw";
