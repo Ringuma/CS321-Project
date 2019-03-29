0a02f68276f625d21d68936221d45111296d60dd
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2019-03-29 14:35:27 -0400

    Updated background.js
    Added code to open a window for popup.html. When alarm is on, an alert pop-ups.

--- js/background.js
+++ js/background.js
@@ -1,1 +1,10 @@
-
+chrome.runtime.onLaunched.addListener(function(){
+	chrome.app.window.create('html/popup.html', {
+			id: 'main',
+			bounds: { width: 500, height: 500 }
+		});
+});
+	chrome.alarms.onAlarm.addListener(function(timer){
+		alert("I am recommending something!");
+	}
+}
