bf140fd1b83eb5eabaef1ea9c71ddc5528d8ff75
Ringuma <doodlgirl863@gmail.com>
2019-03-28 21:35:11 -0400

    Deleted unneeded files and links
    Deleted jso,js, api_key.json, credentials.json and deleted links in popup.html. Commented out testing code in background.js.

--- js/background.js
+++ js/background.js
@@ -1,1 +1,34 @@
+/*
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // ...inject 'jquery.min.js' and...
+    chrome.tabs.executeScript(tab.id, {
+        file: "jquery-3.3.1.js",
+        allFrames: true,
+        runAt: "document_idle"
+    });
 
+    chrome.tabs.executeScript(tab.id, {
+        file: "jso.js",
+        allFrames: true,
+        runAt: "document_idle"
+    });
+
+    chrome.tabs.executeScript(tab.id, {
+        file: "require.js",
+        allFrames: true,
+        runAt: "document_idle"
+    });
+
+    chrome.tabs.executeScript(tab.id, {
+        file: "jquery.csv.js",
+        allFrames: true,
+        runAt: "document_idle"
+    });
+    // ...inject 'content.js' into the active tab's page
+    chrome.tabs.executeScript(tab.id, {
+        file: "recommendation.js",
+        allFrames: true,
+        runAt: "document_idle"
+    });
+});
+*/
