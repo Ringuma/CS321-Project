6276a8e47d9f3470a38d0c0b073f3781eb1cd42b
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2019-04-05 18:15:40 -0400

    Simplified background.js
    have where when the timer goes off, a placeholder alert pops up

--- js/background.js
+++ js/background.js
@@ -1,10 +1,3 @@
-chrome.runtime.onLaunched.addListener(function(){
-	chrome.app.window.create('html/popup.html', {
-			id: 'main',
-			bounds: { width: 500, height: 500 }
-		});
+chrome.alarms.onAlarm.addListener(function(timer) {
+  alert("I work");
 });
-	chrome.alarms.onAlarm.addListener(function(timer){
-		alert("I am recommending something!");
-	}
-}
