1670f2da237163b8083e010da2ed6122d1efb0a6
Ringuma <doodlgirl863@gmail.com>
2019-04-06 21:54:47 -0400

    Recommendation is remembered when closing and reopening popup
    added myStorage variable to refer to localStorage API. Made it so the recommendation is stored and remembered in between sessions of opening the popup

--- js/recommendation.js
+++ js/recommendation.js
@@ -1,12 +1,23 @@
 
 var myStorage = window.localStorage;
 var spreadsheetURL = "../data/myAnimeListData.csv";
-var sheetData = [];
+//var sheetData = [];
+//var Recommendation = null;
 
 // load dataset and compute recommendation when popup loads
 document.addEventListener("DOMContentLoaded", function(){
-  loadData();
-  computeRecommendation();
+
+  // checks to see if recommendation was already computed
+  if (myStorage.getItem("initialized") !== "true") {
+    loadData();
+    //computeRecommendation();
+    console.log("first compute!");
+  }
+  else {
+    displayRec();
+  }
+
+
 }, false);
 
 document.getElementById("refresh_button").addEventListener("click", function() {
@@ -20,8 +31,14 @@ function loadData() {
       url: spreadsheetURL,
       dataType: "text",
       success: function(data) {
-          sheetData = $.csv.toArrays(data);
-          myStorage.setItem("animeData", JSON.stringify(sheetData));
+          myStorage.setItem("animeData", JSON.stringify($.csv.toArrays(data)));
+
+          // first, title; second, genre array; third, link string
+          var Recommendation = [null,null,null]
+          myStorage.setItem("recommendation", JSON.stringify(Recommendation));
+          myStorage.setItem("initialized", "true");
+          console.log(myStorage.getItem("recommendation") + " is initialized == " + myStorage.getItem("initialized"));
+          computeRecommendation();
       },
       error: function (request, status, error) {
           alert(request.responseText);
@@ -29,15 +46,11 @@ function loadData() {
    });
 }
 
- var Recommendation = {
-   title: "",
-   matchingGenres: [],
-   link: "",
- }
 
 
 
 function computeRecommendation() {
+  console.log("in computeRec");
   var animeData = JSON.parse(myStorage.getItem("animeData"));
 
   // generate random index between 1-dataset length (excluse 0th index
@@ -49,24 +62,24 @@ function computeRecommendation() {
   var index = Math.floor(Math.random() * (max - min + 1)) + min;
 
   // build Recommendation object
-  Recommendation.title = animeData[index][1];
-  //alert("Computed");
+  var Recommendation = [animeData[index][1], animeData[index][3], "bla"];
+
+  myStorage.setItem("recommendation", JSON.stringify(Recommendation));
+
   displayRec();
-  //alert("Displayed");
 }
 
 function displayRec() {
-  if (myStorage.getItem("animeData")) {
-    //computeRecommendation();
+  //if (myStorage.getItem("animeData")) {
     document.getElementById("description").innerHTML =
-    "<h1>" + Recommendation.title + "<h1>";
-    //alert("Data replaced");
-  }
-  else {
-    document.getElementById("description").innerHTML = "<p>Database is not available.<p>";
-    alert("Data not loaded");
-  }
-  //alert("At end of displayRec()");
+    "<h1>" + JSON.parse(myStorage.getItem("recommendation"))[0] + "<h1>";
+    console.log("recommendation = " + myStorage.getItem("recommendation") + "\n initialized = " + myStorage.getItem("initialized"));
+    //myStorage.setItem("recommendation", myStorage.getItem("recommendation"));
+  //}
+  //else {
+  //  document.getElementById("description").innerHTML = "<p>Database is not available.<p>";
+  //  alert("Data not loaded");
+  //}
 }
 
 function applyFilters(data, filters) {
