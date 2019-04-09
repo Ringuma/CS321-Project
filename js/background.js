0b83bfb70c08a81ed8e5dc165dd0a3ed366c098f
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2019-04-08 23:07:43 -0400

    Updated background.js to respond to a message
    background.js recieves a message from recommendation.js. and responds. This allows recommendation.js to compute new recommendations in the background when the pop-up is closed.

--- js/background.js
+++ js/background.js
@@ -1,3 +1,8 @@
-chrome.alarms.onAlarm.addListener(function(timer) {
-  alert("You have a new recommendation!"); //placeholder
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  alert("You have a new recommendation! Check the pop-up to view it.");
+  chrome.runtime.onMessage.addListener( //responds to a message from recommendation.js
+	function(request, sender, sendResponse){
+			if(request.check == "here")
+				sendResponse({call: "there"});
+	});
 });
