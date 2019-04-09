d2987f518b409e767f5175a5ea3c485a1c069300
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2019-04-08 21:43:57 -0400

    added comments

--- js/alarms.js
+++ js/alarms.js
@@ -1,22 +1,22 @@
 var timer = {
-	Daily: function(x){
+	Daily: function(x){ //creates the timer for daily periods.
 		chrome.alarms.create("time", {delayInMinutes: 1.0, periodInMinutes:1439.0});
 	},
 	
-	Weekly: function(x){
+	Weekly: function(x){ //creates the timer for weekly periods
 		chrome.alarms.create("time", {delayInMinutes: 1.0, periodInMinutes:10079.0});
 	},
 	
-	Monthly: function(x){
+	Monthly: function(x){ //creates the timer for the monthly periods
 		chrome.alarms.create("time", {delayInMinutes: 1.0, periodInMinutes:43799.0});
 		window.close();	
 	},
-	Off : function(x){
+	Off : function(x){ //removes any active timer.
 		chrome.alarms.clear("time");
 	},
-	setup: function(){
+	setup: function(){ //setups which function is performed based on the id of the button in options.html
 		var t = document.getElementById('daily');
-		t.addEventListener('click', timer.Daily);
+		t.addEventListener('click', timer.Daily); //when a 'click' occurs for the button, the respective function is performed.
 		var t = document.getElementById('weekly');
 		t.addEventListener('click', timer.Weekly);
 		var t = document.getElementById('monthly');
@@ -26,6 +26,6 @@ var timer = {
 	}
 };
 
-document.addEventListener('DOMContenetLoaded', function(){
+document.addEventListener('DOMContenetLoaded', function(){ //adds an event listener that always goes to timer.setup function
 	timer.setup();
 });
