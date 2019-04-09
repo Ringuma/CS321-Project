b2f23dd195d93b9490f9b263fdda40262d6ccc4c
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2019-04-08 20:52:38 -0400

    Updated alarms.js with basic functionality
    alarms.js's basic functions of creating an "alarm" that goes off periodically daily, weekly, or monthly has been added. An Off function has also been made. Fixed the error causing the functions not to work.

--- js/alarms.js
+++ js/alarms.js
@@ -1,26 +1,28 @@
 var timer = {
 	Daily: function(x){
-		chrome.alarms.create("time", {delayInMinues: 0.1, periodInMinutes:0.2});
-		window.close();
+		chrome.alarms.create("time", {delayInMinutes: 1.0, periodInMinutes:1439.0});
 	},
 	
 	Weekly: function(x){
-		chrome.alarms.create("time", {delayInMinues: 0.3, periodInMinutes:0.4});
-		window.close();
+		chrome.alarms.create("time", {delayInMinutes: 1.0, periodInMinutes:10079.0});
 	},
 	
 	Monthly: function(x){
-		chrome.alarms.create("time", {delayInMinues: 0.5, periodInMinutes:0.6});
+		chrome.alarms.create("time", {delayInMinutes: 1.0, periodInMinutes:43799.0});
 		window.close();	
 	},
-	
+	Off : function(x){
+		chrome.alarms.clear("time");
+	},
 	setup: function(){
-		var t = document.getElementById('Daily');
+		var t = document.getElementById('daily');
 		t.addEventListener('click', timer.Daily);
-		var t = document.getElementById('Weekly');
+		var t = document.getElementById('weekly');
 		t.addEventListener('click', timer.Weekly);
-		var t = document.getElementById('Monthly');
+		var t = document.getElementById('monthly');
 		t.addEventListener('click', timer.Monthly);
+		var t = document.getElementById('off');
+		t.addEventListener('click', timer.Off);
 	}
 };
 
