002e00068e3e8677026ab0cd30f67999d9b15d4a
Ringuma <doodlgirl863@gmail.com>
2019-04-12 00:41:27 -0400

    Error message and callback function addition
    Added an error message that displays when the user selects a combination of filters that doesn't match any anime.
    Also added callback functionality to make sure functions are executed in order.

--- js/recommendation.js
+++ js/recommendation.js
@@ -6,7 +6,9 @@ var spreadsheetURL = "../data/myAnimeListData.csv";
 document.addEventListener("DOMContentLoaded", function(){
   // load dataset  when popup opens for the first time
   if (myStorage.getItem("initialized") !== "true") {
-    loadData(); // loads dataset, which then calls computeRecommendation(), which then calls displayRec()
+    loadData( function() {
+      computeRecommendation(displayRec);
+    }); // loads dataset, which then calls computeRecommendation(), which then calls displayRec()
   }
   else {
     displayRec(); // displays previous recommendation
@@ -15,11 +17,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // event listener that computes a new recommendation upon clicking the refresh button
 document.getElementById("refresh_button").addEventListener("click", function() {
-   computeRecommendation();
+   computeRecommendation(displayRec);
 });
 
 // function that loads spreadsheet data
-function loadData() {
+function loadData(callback) {
   $.ajax({
       type: "GET",
       url: spreadsheetURL,
@@ -49,56 +51,69 @@ function loadData() {
           var filters = [];
           myStorage.setItem("currentFilters", JSON.stringify(filters));
 
-          // console.log(myStorage.getItem("recommendation") + " is initialized == " + myStorage.getItem("initialized"));
-          computeRecommendation(); // computes initial recommendation
+          //computeRecommendation(); // computes initial recommendation
+
       },
       error: function (request, status, error) {
           alert(request.responseText);
       }
    });
+   callback();
 }
 
 
 
 // this function computes a random recommendation based on given inputs
-function computeRecommendation() {
+function computeRecommendation(callback) {
   // 2d array that represents current dataset for anime
   var animeData = JSON.parse(myStorage.getItem("filteredData"));
+  //alert(animeData[0]);
 
-  // generate random index between 1-dataset length (exclude 0th index
-  // because the 0th index is the header for the dataset)
-  var min = 1;
-  var max = animeData.length;
+  // check if filteredData is empty------------------------------------------------
+  if (animeData.length == 0) {
+    myStorage.setItem("emptyDataSet", true);
+    displayRec(); // returns before displaying rec?
+  }
+  else {
+    myStorage.setItem("emptyDataSet", false);
+    // generate random index between 1-dataset length (exclude 0th index
+    // because the 0th index is the header for the dataset)
+    var min = 1;
+    var max = animeData.length;
 
-  // calculates random index 1 to animeData.length inclusive
-  var index = Math.floor(Math.random() * (max - min + 1)) + min;
+    // calculates random index 1 to animeData.length inclusive
+    var index = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  // build Recommendation object
-  var Recommendation = [animeData[index][1], animeData[index][3], animeData[index][0]];
+    // build Recommendation object
+    // attributes: title, genre, ID, year, rating, ep count, studio
+    var Recommendation = [animeData[index][1], animeData[index][3], animeData[index][0], animeData[index][2], animeData[index][10],animeData[index][5],animeData[index][8]];
 
-  // saves current recommendation in localStorage
-  myStorage.setItem("recommendation", JSON.stringify(Recommendation));
-
-  // displays the recommendation
-  displayRec();
+    // saves current recommendation in localStorage
+    myStorage.setItem("recommendation", JSON.stringify(Recommendation));
+  }
+  callback();
 }
 
 // this function displays the current recommendation in the popup
 function displayRec() {
-    var recommendation = JSON.parse(myStorage.getItem("recommendation"));
-    // changes the popup HTML to reflect current recommendation
-    document.getElementById("description").innerHTML =
-    `<h1>Your Recommendation</h1>
-    <p>Title: ${recommendation[0]}<p>
-    <p>Genre: ${recommendation[1]}</p>
-    <a target=\"_blank\" href=\"https://myanimelist.net/anime/${recommendation[2]}/${recommendation[0]}\">MAL Link.</a>`;
+    if (myStorage.getItem("emptyDataSet") === "true") { // dataset is empty, display error message
+      document.getElementById("description").innerHTML =
+      `<h1>I'm Sorry</h1>
+      <p>There is no anime that matches your choice of filters.
+      Please go to the Settings page and choose a different set of filters.</p>`;
+    }
+    else {
+      var recommendation = JSON.parse(myStorage.getItem("recommendation"));
+      // changes the popup HTML to reflect current recommendation
+      document.getElementById("description").innerHTML =
+      `<h1>Your Recommendation</h1>
+      <p>Title: ${recommendation[0]}<p>
+      <p>Genre: ${recommendation[1]}</p>
+      <p>Season: ${recommendation[3]}</p>
+      <p>Rating: ${recommendation[4]}</p>
+      <p>Episode Count: ${recommendation[5]}</p>
+      <p>Studio: ${recommendation[6]}</p>
+      <a target=\"_blank\" href=\"https://myanimelist.net/anime/${recommendation[2]}/${recommendation[0]}\">MAL Link.</a>`;
+    }
+    //callback();
 }
-
-
-/*
-document.addEventListener("load", function() {
-  computeRecommendation();
-});
-*/
-//var clientID = "983014862567-srsvv1p8djudqjalo4onabl8qog48mdi.apps.googleusercontent.com";
-//var spreadsheetID = "1brguO5nGfXS-Fr1Xcf3pqPTQoBUPGLTYM_EMAA9yJFw";
