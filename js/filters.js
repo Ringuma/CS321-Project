8bb8c82929406285951705934667747c8d93ae17
Ringuma <doodlgirl863@gmail.com>
2019-04-11 23:11:31 -0400

    Applied restrictive filter logic
    Entries are only added to filteredData array if they match all of the chosen filters. Changed the value attributes of the options.html file to aid in retrieval of checkboxes that represent a range of values (e.g. 2007-2010).

--- js/filters.js
+++ js/filters.js
@@ -34,62 +34,80 @@ function applyFilters() {
   // operates on the logic where if the entry matches 1 or more filters, it will be added to the filterArray
   // brute force method, needs to be optimized
   for (var i = 1; i < animeData.length; i++) { // starts at index 1 because index 0 is table header
-    // genre
-    var found = false;
-    for (var j = 0; j < currentFilters[0].length; j++) {
-      if (animeData[i][3].indexOf(currentFilters[0][j]) != -1) { // index isn't -1, so exists in array
-        filteredData.push(animeData[i]);
-        found = true;
 
-        break;
+    // only adds anime to "filteredData" if it matches all selected filters
+    var found = true;
+
+    // genre-------------------------------------------------------------------------------------------------
+    if (currentFilters[0].length !== 0) {
+      var entryGenres = animeData[i][3];
+
+      for (var j = 0; j < currentFilters[0].length; j++) {
+        if (!(entryGenres.indexOf(currentFilters[0][j]) != -1)) { // index isn't -1, so exists in array
+          found = false;
+        }
       }
     }
 
-    // checks if found in previous loop
-    if (found) {
-      continue;
+    // rating------------------------------------------------------------------------------------------------
+    if (currentFilters[1].length !== 0) {
+      console.log("Searching for rating filters..")
+      //for (var j = 0; j < currentFilters[1].length; j++) {
+        var entryRating = animeData[i][10];
+        var filterRatingMin = currentFilters[1][0];
+        var filterRatingMax = currentFilters[1][2];
+
+        if (! (entryRating >= filterRatingMin) & ! (entryRating <= filterRatingMax)) {
+          found = false;
+        }
+      //}
     }
 
-    // rating
-    if (animeData[i][10] == currentFilters[1]) { // rating located at 10th index of anime entry
-      filteredData.push(animeData[i]);
+    // studio-------------------------------------------------------------------------------------------------
+    if (currentFilters[2].length !== 0) {
+      var entryStudios = animeData[i][8];
 
-      continue;
-    }
-
-    // studio
-    for (var j = 0; j < currentFilters[2].length; j++) {
-      if (animeData[i][8].indexOf(currentFilters[2][j]) != -1) { // index isn't -1 so exists in array
-        filteredData.push(animeData[i]);
-        found = true;
-
-        break;
+      for (var j = 0; j < currentFilters[2].length; j++) {
+        if (!(entryStudios.indexOf(currentFilters[2][j]) != -1)) { // index isn't -1, so exists in array
+          found = false;
+        }
       }
     }
 
-    // checks if found in previous loop
+    // ep count-------------------------------------------------------------------------------------------------
+    if (currentFilters[3].length !== 0) {
+      //for (var j = 0; j < currentFilters[3].length; j++) {
+        var entryEpCount = animeData[i][5];
+        var filterCountMin = currentFilters[3][0];
+        var filterCountMax = currentFilters[3][1];
+
+        if (! (entryEpCount >= filterCountMin && entryEpCount <= filterCountMax)) {
+          found = false;
+        }
+      //}
+    }
+
+    // year-------------------------------------------------------------------------------------------------
+    if (currentFilters[4].length !== 0) {
+      //for (var j = 0; j < currentFilters[4].length; j++) {
+        var entryYear = animeData[i][2].split(" ")[1];
+        var filterYearMin = currentFilters[4][0];
+        var filterYearMax = currentFilters[4][1];
+
+        if (! (entryYear >= filterYearMin && entryYear <= filterYearMax)) {
+          found = false;
+        }
+      //}
+    }
+
+    // passed all tests?-------------------------------------------------------------------------------------------------
     if (found) {
-      continue;
-    }
-
-    // ep Count
-    if (animeData[i][5] == currentFilters[3]) {
       filteredData.push(animeData[i]);
-
-      continue;
     }
-
-    // year
-    if (animeData[i][2].includes(currentFilters[4])) {
-      filteredData.push(animeData[i]);
-
-      continue;
-    }
-
   }
 
   myStorage.setItem("filteredData", JSON.stringify(filteredData));
-  alert("Filters applied! Please refresh your recommendation for changes to be applied." + filteredData);
+  alert("Filters applied! Please refresh your recommendation for changes to be applied.\n" + filteredData);
 }
 
 // checks to see which filter checkboxes are checked when you click "Apply" in the options.html page
@@ -100,20 +118,52 @@ function parseFilters() {
 
   for (var i = 0; i < checkedBoxes.length; i++) {
     // search for name substring to put genre in appropriate subarray
+
+    // genre ------------------------------------------------------------------------------------------------
     if (checkedBoxes[i].name.includes("genre")) {
       filterArray[0].push(checkedBoxes[i].value);
     }
-    else if (checkedBoxes[i].name.includes("rating")) {
-      filterArray[1].push(checkedBoxes[i].value);
+
+    // rating ------------------------------------------------------------------------------------------------
+    else if (checkedBoxes[i].name.includes("rating")) { // range, only record lowest and highest VALUES
+      if (filterArray[1].length == 0) {
+        filterArray[1].push(checkedBoxes[i].getAttribute("value1"));
+        filterArray[1].push(checkedBoxes[i].getAttribute("value2"));
+      }
+      else { // if new max is encountered, replaces max value
+        filterArray[1].pop();
+        filterArray[1].push(checkedBoxes[i].getAttribute("value2"));
+      }
+
     }
+
+    // studio ------------------------------------------------------------------------------------------------
     else if (checkedBoxes[i].name.includes("studio")) {
       filterArray[2].push(checkedBoxes[i].value);
     }
+
+    // episode count ------------------------------------------------------------------------------------------------
     else if (checkedBoxes[i].name.includes("ecount")) {
-      filterArray[3].push(checkedBoxes[i].value);
+      if (filterArray[3].length == 0) {
+        filterArray[3].push(checkedBoxes[i].getAttribute("value1"));
+        filterArray[3].push(checkedBoxes[i].getAttribute("value2"));
+      }
+      else { // if new max is encountered, replaces max value
+        filterArray[3].pop();
+        filterArray[3].push(checkedBoxes[i].getAttribute("value2"));
+      }
     }
+
+    // year ------------------------------------------------------------------------------------------------
     else if (checkedBoxes[i].name.includes("year")) {
-      filterArray[4].push(checkedBoxes[i].value);
+      if (filterArray[4].length == 0) {
+        filterArray[4].push(checkedBoxes[i].getAttribute("value1"));
+        filterArray[4].push(checkedBoxes[i].getAttribute("value2"));
+      }
+      else { // if new max is encountered, replaces max value
+        filterArray[4].pop();
+        filterArray[4].push(checkedBoxes[i].getAttribute("value2"));
+      }
     }
 
   }
