6a93af9895d2a8851c67bddc3be663aa4883ef5d
michaelphamct <47987943+michaelphamct@users.noreply.github.com>
2019-04-14 00:53:42 -0400

    Apply the font size to the whole options page

--- js/fontsize.js
+++ js/fontsize.js
@@ -1,6 +1,17 @@
 var myStorage = window.localStorage;
+var slider = document.getElementById("sizeSlider");
+var output = document.getElementById("fontSize");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
 function applyFontSize() {
+  var sizeValue = slider.value;
+  myStorage.setItem("fontSize", sizeValue);
+  document.body.style.fontSize = myStorage.getItem("fontSize");
 }
 
 document.getElementsByName("Apply")[0].addEventListener("click", function() {
