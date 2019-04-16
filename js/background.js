8b49f663583ddeb2247123f3ffd90604887c491f
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2019-04-15 23:16:37 -0400

    Added code for intial install recommendation time
    Code added causes an alarm to be created automatically when the user installs the extension. It is set daily, but can be changed by the user in the options page.

--- js/background.js
+++ js/background.js
@@ -1,3 +1,7 @@
+chrome.runtime.onInstalled.addListener(function(){ //automatically sets a daily frequency when the extension is first installed.
+		chrome.alarms.create("time", {delayInMinutes: 1440.0, periodInMinutes:1440.0});
+});
+
 chrome.alarms.onAlarm.addListener(function(alarm) {
   alert("You have a new recommendation! Check the pop-up to view it.");
   chrome.runtime.onMessage.addListener( //responds to a message from recommendation.js
