a6a2e6bd626f219fedff9374bd22a81289315961
michaelphamct <47987943+michaelphamct@users.noreply.github.com>
2019-04-17 11:51:23 -0400

    Switch statements for each color scheme

--- js/colorscheme.js
+++ js/colorscheme.js
@@ -1,8 +1,44 @@
 function applyColorScheme() {
   var myStorage = window.localStorage;
 
-  colorChosen = document.querySelector('input[name="colors"]:checked').value;
+  var colorChosen = document.querySelector('input[name="colors"]:checked').value;
   myStorage.setItem("color", colorChosen);
+  colorChosen = myStorage.getItem("color");
+  switch (colorChosen) {
+    default:
+    case "color1":
+      break;
+    case "color2":
+      break;
+    case "color3":
+      break;
+    case "color4":
+      break;
+    case "color5":
+      break;
+    case "color6":
+      break;
+    case "color7":
+      break;
+    case "color8":
+      break;
+    case "color9":
+      break;
+    case "color10":
+      break;
+    case "color11":
+      break;
+    case "color12":
+      break;
+    case "color13":
+      break;
+    case "color14":
+      break;
+    case "color15":
+      break;
+    case "color16":
+      break;
+  }
 }
 
 // add event listener to apply button and run the applyColorScheme() function
