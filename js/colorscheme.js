72e9644aa7a1f236d9d2c681a0721fb0e5ffdf85
michaelphamct <47987943+michaelphamct@users.noreply.github.com>
2019-04-18 00:04:51 -0400

    Preserve color settings after opening + add styles

--- js/colorscheme.js
+++ js/colorscheme.js
@@ -1,3 +1,13 @@
+if (window.localStorage.getItem("color") === null) {
+  window.localStorage.setItem("color", "color1");
+}
+
+var savedColor = window.localStorage.getItem("color");
+
+document.getElementById(savedColor).checked = true;
+
+applyColorScheme();
+
 function applyColorScheme() {
   var colorChosen = document.querySelector('input[name="colors"]:checked').value;
   var colorSheet = document.getElementById("colorsheet");
@@ -13,32 +23,46 @@ function applyColorScheme() {
       colorSheet.setAttribute("href", "../css/color2.css");
       break;
     case "color3":
+      colorSheet.setAttribute("href", "../css/color3.css");
       break;
     case "color4":
+      colorSheet.setAttribute("href", "../css/color4.css");
       break;
     case "color5":
+      colorSheet.setAttribute("href", "../css/color5.css");
       break;
     case "color6":
+      colorSheet.setAttribute("href", "../css/color6.css");
       break;
     case "color7":
+      colorSheet.setAttribute("href", "../css/color7.css");
       break;
     case "color8":
+      colorSheet.setAttribute("href", "../css/color8.css");
       break;
     case "color9":
+      colorSheet.setAttribute("href", "../css/color9.css");
       break;
     case "color10":
+      colorSheet.setAttribute("href", "../css/color10.css");
       break;
     case "color11":
+      colorSheet.setAttribute("href", "../css/color11.css");
       break;
     case "color12":
+      colorSheet.setAttribute("href", "../css/color12.css");
       break;
     case "color13":
+      colorSheet.setAttribute("href", "../css/color13.css");
       break;
     case "color14":
+      colorSheet.setAttribute("href", "../css/color14.css");
       break;
     case "color15":
+      colorSheet.setAttribute("href", "../css/color15.css");
       break;
     case "color16":
+      colorSheet.setAttribute("href", "../css/color16.css");
       break;
   }
 }
