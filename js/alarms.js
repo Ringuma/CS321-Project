bbcf570421949225d8a87a60b1457f13186ecda5
Ringuma <doodlgirl863@gmail.com>
2019-04-19 14:59:06 -0400

    Fixed typo causing timer to not work
    At end, 'DOMContenetLoaded' to 'DOMContentLoaded'

--- js/alarms.js
+++ js/alarms.js
@@ -3,12 +3,12 @@ var timer = {
 		chrome.alarms.create("time", {delayInMinutes: 1440.0, periodInMinutes:1440.0});
 		alert("Daily Frequency On");
 	},
-	
+
 	Weekly : function(x){ //sets the alarm to a weekly cycle
 		chrome.alarms.create("time", {delayInMinutes: 10080.0, periodInMinutes:10080.0});
 		alert("Weekly Frequency On");
 	},
-	
+
 	Monthly : function(x){ //sets the alarm to a monthly (30 day) cycle
 		chrome.alarms.create("time", {delayInMinutes: 438000.0, periodInMinutes:43800.0});
 		alert("Monthly Frequency On");
@@ -22,19 +22,19 @@ var timer = {
 		alert("Recommendation Frequency Off");
 	},
 	setup: function(){ //setups which function is performed based on the id of the button in options.html
-		var t = document.getElementById('daily');
-		t.addEventListener('click', timer.Daily); //when a 'click' occurs for the button, the respective function is performed.
-		var t = document.getElementById('weekly');
-		t.addEventListener('click', timer.Weekly);
-		var t = document.getElementById('monthly');
-		t.addEventListener('click', timer.Monthly);
-		var t = document.getElementById('test'); //adds a event listener to allow for test to timer
-		t.addEventListener('click', timer.Tester );
-		var t = document.getElementById('off');
-		t.addEventListener('click', timer.Off);
+		var a = document.getElementById('daily');
+		a.addEventListener('click', timer.Daily); //when a 'click' occurs for the button, the respective function is performed.
+		var b = document.getElementById('weekly');
+		b.addEventListener('click', timer.Weekly);
+		var c = document.getElementById('monthly');
+		c.addEventListener('click', timer.Monthly);
+		var d = document.getElementById('test'); //adds a event listener to allow for test to timer
+		d.addEventListener('click', timer.Tester );
+		var e = document.getElementById('off');
+		e.addEventListener('click', timer.Off);
 	}
 };
 
-document.addEventListener('DOMContenetLoaded', function(){ //adds an event listener that always goes to timer.setup function
+document.addEventListener('DOMContentLoaded', function(){ //adds an event listener that always goes to timer.setup function
 	timer.setup();
 });
